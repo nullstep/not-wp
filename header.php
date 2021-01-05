cf63e7e1b318f264dee835bbeb1673a9729274e2
nullstep <me@nullstep.com>
2021-01-05 12:57:10 +0000

    update bootstrap and scripts

--- header.php
+++ header.php
@@ -3,11 +3,11 @@
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
 	<meta name="description" content="<?php bloginfo('description'); ?>">
 	<title><?php echo trim(wp_title('', false)); ?><?php if (wp_title('', false)) { echo ' : '; } ?><?php bloginfo('name'); ?></title>
 	<link href="/uploads/<?php getvalue('favicon_image'); ?>" rel="shortcut icon">
-	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <?php wp_head(); ?>
 	<style>
 		:root {
