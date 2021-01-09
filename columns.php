db0b32feb4254cb63e49e893664ecf8b93d23a2b
nullstep <me@nullstep.com>
2021-01-09 11:18:29 +0000

    sidebar template

---
+++ columns.php
@@ -0,0 +1,21 @@
+<?php /* Template Name: Full Width template */ ?>
+<?php setviews(get_the_ID()); ?>
+<?php get_header(); ?>
+
+			<main class="row">
+				<section class="col-sm-12">
+					<h2><?php the_title(); ?></h2>
+<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+					<article>
+<?php the_content(); ?>
+
+					</article>
+<?php endwhile; ?>
+<?php else: ?>
+					<article>
+						<h2>No Content</h2>
+					</article>
+<?php endif; ?>
+				</section>
+			</main>
+<?php get_footer(); ?>
